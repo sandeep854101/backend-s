0dd4aae5f850dc03b70516a0b744f5d55a1c3b12
SANDEEP KUMAR <139211042+sandeep854101@users.noreply.github.com>
2024-12-23 15:38:00 +0530

    fsd

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,15 @@
-// config/db.js
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      connectTimeoutMS: 30000,
     });
-    console.log("MongoDB connected");
+    console.log("Connected to MongoDB Atlas");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1); // Exit process with failure
   }
 };
 
